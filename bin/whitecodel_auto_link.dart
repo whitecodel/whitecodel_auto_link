94e1e9454ae00f6f2a7cce2c843e7af966027701
whitecodel <bhawanishankar1308@gmail.com>
2024-01-11 18:06:44 +0530

    colors added for logs

--- bin/whitecodel_auto_link.dart
+++ bin/whitecodel_auto_link.dart
@@ -1,35 +1,38 @@
 import 'package:whitecodel_auto_link/whitecodel_auto_link.dart';
+import 'package:chalkdart/chalk.dart';
+
+var error = chalk.bold.red;
+var info = chalk.bold.blue;
 
 void main(List<String> arguments) {
   if (arguments.isEmpty) {
-    print('Please provide the token like this: whitecodel_auto_link <token>');
-    print(
-        'To obtain your Diawi token, visit https://dashboard.diawi.com/profile/api');
-
+    print(error(
+        'Error: Please provide the token like this: ${chalk.yellow('whitecodel_auto_link <token>')}'));
+    print(info(
+        'Info: To obtain your Diawi token, visit ${chalk.green.underline('https://dashboard.diawi.com/profile/api')}'));
     return;
   }
-  print('Starting Whitecodel Auto Link... 🚀');
+
+  print(info('Info: Starting Whitecodel Auto Link... 🚀'));
+
   if (arguments.length == 1) {
     startProcess(arguments[0]);
   } else if (arguments.length == 2) {
     if (!['apk', 'ipa', 'both'].contains(arguments[1])) {
-      print(
-          'Please provide the build type like this: whitecodel_auto_link <token> <buildType>');
-      print('Valid build types are: apk, ipa, both');
+      print(error(
+          'Error: Invalid build type. Valid build types are: ${chalk.yellow('apk, ipa, both')}'));
       return;
     }
     startProcess(arguments[0], buildType: arguments[1], releaseType: 'debug');
   } else if (arguments.length == 3) {
     if (!['apk', 'ipa', 'both'].contains(arguments[1])) {
-      print(
-          'Please provide the build type like this: whitecodel_auto_link <token> <buildType> <releaseType>');
-      print('Valid build types are: apk, ipa, both');
+      print(error(
+          'Error: Invalid build type. Valid build types are: ${chalk.yellow('apk, ipa, both')}'));
       return;
     }
     if (!['debug', 'release'].contains(arguments[2])) {
-      print(
-          'Please provide the release type like this: whitecodel_auto_link <token> <buildType> <releaseType>');
-      print('Valid release types are: debug, release');
+      print(error(
+          'Error: Invalid release type. Valid release types are: ${chalk.yellow('debug, release')}'));
       return;
     }
     startProcess(arguments[0],
--- lib/whitecodel_auto_link.dart
+++ lib/whitecodel_auto_link.dart
@@ -1,37 +1,40 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'dart:io';
+import 'package:chalkdart/chalk.dart';
+
+var error = chalk.bold.red;
+var info = chalk.bold.blue;
 
 void main(List<String> arguments) {
   if (arguments.isEmpty) {
-    print('Please provide the token like this: whitecodel_auto_link <token>');
-    print(
-        'To obtain your Diawi token, visit https://dashboard.diawi.com/profile/api');
-
+    print(error(
+        'Error: Please provide the token like this: ${chalk.yellow('whitecodel_auto_link <token>')}'));
+    print(info(
+        'Info: To obtain your Diawi token, visit https://dashboard.diawi.com/profile/api'));
     return;
   }
-  print('Starting Whitecodel Auto Link... 🚀');
+
+  print(info('Info: Starting Whitecodel Auto Link... 🚀'));
+
   if (arguments.length == 1) {
     startProcess(arguments[0]);
   } else if (arguments.length == 2) {
     if (!['apk', 'ipa', 'both'].contains(arguments[1])) {
-      print(
-          'Please provide the build type like this: whitecodel_auto_link <token> <buildType>');
-      print('Valid build types are: apk, ipa, both');
+      print(error(
+          'Error: Invalid build type. Valid build types are: ${chalk.yellow('apk, ipa, both')}'));
       return;
     }
     startProcess(arguments[0], buildType: arguments[1], releaseType: 'debug');
   } else if (arguments.length == 3) {
     if (!['apk', 'ipa', 'both'].contains(arguments[1])) {
-      print(
-          'Please provide the build type like this: whitecodel_auto_link <token> <buildType> <releaseType>');
-      print('Valid build types are: apk, ipa, both');
+      print(error(
+          'Error: Invalid build type. Valid build types are: ${chalk.yellow('apk, ipa, both')}'));
       return;
     }
     if (!['debug', 'release'].contains(arguments[2])) {
-      print(
-          'Please provide the release type like this: whitecodel_auto_link <token> <buildType> <releaseType>');
-      print('Valid release types are: debug, release');
+      print(error(
+          'Error: Invalid release type. Valid release types are: ${chalk.yellow('debug, release')}'));
       return;
     }
     startProcess(arguments[0],
@@ -39,164 +42,164 @@ void main(List<String> arguments) {
   }
 }
 
-startProcess(diawiToken, {buildType = 'both', releaseType = 'debug'}) async {
+void startProcess(diawiToken,
+    {buildType = 'both', releaseType = 'debug'}) async {
   try {
     var finalResult = [];
+
     if (buildType == 'apk' || buildType == 'both') {
       await buildApk(releaseType);
       var uploadResult = await uploadToDiawi(diawiToken,
           'build/app/outputs/apk/release/app-$releaseType.apk', 'APK');
       var jobId = uploadResult['job'];
       var statusResult = await checkDiawiStatus(jobId, diawiToken);
+
       while (statusResult['status'] != 2000) {
         await Future.delayed(Duration(seconds: 5));
         statusResult = await checkDiawiStatus(jobId, diawiToken);
       }
+
       finalResult.add({
         'for': 'APK',
         'link': statusResult['link'],
         'icon': '🤖',
       });
     }
+
     if (buildType == 'ipa' || buildType == 'both') {
       await buildIPA(releaseType);
       var uploadResult = await uploadToDiawi(diawiToken, 'Runner.ipa', 'IPA');
       var jobId = uploadResult['job'];
       var statusResult = await checkDiawiStatus(jobId, diawiToken);
+
       while (statusResult['status'] != 2000) {
         await Future.delayed(Duration(seconds: 5));
         statusResult = await checkDiawiStatus(jobId, diawiToken);
       }
-      finalResult.add(
-        {
-          'for': 'IPA',
-          'link': statusResult['link'],
-          'icon': '',
-        },
-      );
+
+      finalResult.add({
+        'for': 'IPA',
+        'link': statusResult['link'],
+        'icon': '',
+      });
     }
-    // remove Payload
+
     await Directory('Payload').delete(recursive: true);
-    // remove Runner.ipa
     await File('Runner.ipa').delete();
+
+    print('\n');
+
     for (var result in finalResult) {
-      print('Link for ${result['for']}: ${result['link']} ${result['icon']}');
+      print(info(
+          'Info: Link for ${result['for']}: ${chalk.green.underline(result['link'])} ${chalk.green(result['icon'])}'));
     }
-    print(
-        'Like the package? Please give it a 👍 here: https://pub.dev/packages/whitecodel_auto_link');
+
+    print('\n\n');
+
+    print(info(
+        'Info: Like the package? Please give it a 👍 here: ${chalk.green.underline('https://pub.dev/packages/whitecodel_auto_link')}'));
   } catch (e) {
-    print('Error: $e');
+    print(error('Error: $e'));
   }
 }
 
 Future<void> buildApk(releaseType) async {
-  // build emoji
-  print('Building $releaseType APK... 🔨');
+  print(info('Info: Building $releaseType APK... 🔨'));
   var process =
       await Process.start('flutter', ['build', 'apk', '--$releaseType']);
 
-  // Capture and print the output of the command
   process.stdout.transform(utf8.decoder).listen((data) {
     print(data);
   });
 
   process.stderr.transform(utf8.decoder).listen((data) {
-    print('stderr: $data');
+    print(error('Error: $data'));
   });
 
-  // Wait for the process to complete
   var exitCode = await process.exitCode;
   if (exitCode != 0) {
-    throw 'Command failed with exit code $exitCode';
+    throw error('Error: Command failed with exit code $exitCode');
   }
 
-  print('APK Build Completed Successfully');
+  print(info('Info: APK Build Completed Successfully'));
 }
 
 Future<void> buildIPA(releaseType) async {
-  // check .gitignore exists
   bool isGitIgnoreExists = await File('.gitignore').exists();
+
   if (isGitIgnoreExists) {
-    // check Payload exists in .gitignore
     var gitIgnoreFile = await File('.gitignore').readAsString();
+
     if (!gitIgnoreFile.contains('Payload')) {
-      // add Payload to .gitignore
       await File('.gitignore').writeAsString('$gitIgnoreFile\nPayload');
     }
+
     if (!gitIgnoreFile.contains('Runner.ipa')) {
-      // add Runner.ipa to .gitignore
       await File('.gitignore').writeAsString('$gitIgnoreFile\nRunner.ipa');
     }
   }
-  // mkdir directory Payload
+
   if (await Directory('Payload').exists()) {
     await Directory('Payload').delete(recursive: true);
   }
   await Directory('Payload').create();
 
-  // build emoji
-  print('Building $releaseType IPA... 🔨');
+  print(info('Info: Building $releaseType IPA... 🔨'));
   var process =
       await Process.start('flutter', ['build', 'ios', '--$releaseType']);
 
-  // Capture and print the output of the command
   process.stdout.transform(utf8.decoder).listen((data) {
     print(data);
   });
 
   process.stderr.transform(utf8.decoder).listen((data) {
-    print('stderr: $data');
+    print(error('Error: $data'));
   });
 
-  // Wait for the process to complete
   var exitCode = await process.exitCode;
   if (exitCode != 0) {
-    throw 'Command failed with exit code $exitCode';
+    throw error('Error: Command failed with exit code $exitCode');
   }
 
-  // copy all contents from build/ios/iphoneos/Runner.app to Payload
   var process2 = await Process.start(
       'cp', ['-r', 'build/ios/iphoneos/Runner.app', 'Payload/Runner.app']);
 
-  // Capture and print the output of the command
   process2.stdout.transform(utf8.decoder).listen((data) {
     print(data);
   });
 
   process2.stderr.transform(utf8.decoder).listen((data) {
-    print('stderr: $data');
+    print(error('Error: $data'));
   });
 
   var exitCode2 = await process2.exitCode;
 
   if (exitCode2 != 0) {
-    throw 'Command failed with exit code $exitCode2';
+    throw error('Error: Command failed with exit code $exitCode2');
   }
 
-  // zip Payload
   var process3 = await Process.start('zip', ['-vr', 'Runner.ipa', 'Payload']);
 
-  // Capture and print the output of the command
   process3.stdout.transform(utf8.decoder).listen((data) {
     print(data);
   });
 
   process3.stderr.transform(utf8.decoder).listen((data) {
-    print('stderr: $data');
+    print(error('Error: $data'));
   });
 
   var exitCode3 = await process3.exitCode;
 
   if (exitCode3 != 0) {
-    throw 'Command failed with exit code $exitCode3';
+    throw error('Error: Command failed with exit code $exitCode3');
   }
 
-  print('IPA Build Completed Successfully');
+  print(info('Info: IPA Build Completed Successfully'));
 }
 
 Future<Map<String, dynamic>> uploadToDiawi(diawiToken, path, buildType) async {
-  print('Your Diawi Token: $diawiToken');
-  print('Uploading ${buildType.toUpperCase()} to Diawi... 🚀');
+  print(info('Info: Your Diawi Token: ${chalk.yellow(diawiToken)}'));
+  print(info('Info: Uploading ${buildType.toUpperCase()} to Diawi... 🚀'));
   var diawiUploadUrl = 'https://upload.diawi.com/';
 
   var request = http.MultipartRequest('POST', Uri.parse(diawiUploadUrl))
@@ -214,7 +217,7 @@ Future<Map<String, dynamic>> uploadToDiawi(diawiToken, path, buildType) async {
 }
 
 Future<Map<String, dynamic>> checkDiawiStatus(String jobId, diawiToken) async {
-  print('Checking Diawi Status... 🕵️‍♂️');
+  print(info('Info: Checking Diawi Status... 🕵️‍♂️'));
   var diawiStatusUrl =
       'https://upload.diawi.com/status?token=$diawiToken&job=$jobId';
 
